fae0e5f17d575050f44a400a281dd83965056bc9
Jacob <jra457@msstate.edu>
2023-04-15 12:42:17 -0500

    Update bot code

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="$", intents=intents)
 
