3a9e3691f44c2e335d30621bc8182d4a17fba68a
Jacob <jra457@msstate.edu>
2023-04-14 22:26:39 -0500

    Update bot code

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
 import os
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True  # Add this line to enable the message content intent
+intents.typing = False
+intents.presences = False
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="$", intents=intents)
 
-
 @bot.event
 async def on_ready():
-  print(f"{bot.user} is online!")
-
+    print(f"{bot.user} is online!")
 
 @bot.event
 async def on_message(message):
-  if message.author == bot.user:
-    return
+    if message.author == bot.user:
+        return
 
-  print(
-    f"Message: {message.content}\nUser: {message.author}\nChannel: #{message.channel}"
-  )
+    print(
+        f"Message: {message.content}\nUser: {message.author}\nChannel: #{message.channel}"
+    )
 
-  await bot.process_commands(message)
+    await bot.process_commands(message)
 
-
-@bot.command()  # Change the command name to 'turtle'
-async def turtle(ctx):
-  if ctx.channel.name != 'memes':  # Check if the command was sent in the 'test' channel
-    return
-
-  role_name = "Turtle"
-  role = discord.utils.get(ctx.guild.roles, name=role_name)
-
-  if not role:
-    try:
-      role = await ctx.guild.create_role(name=role_name)
-      print(f"Created new role {role_name}")
-    except discord.Forbidden:
-      return
-
-  if role not in ctx.author.roles:
-    try:
-      await ctx.author.add_roles(role)
-    except discord.Forbidden:
-      return
-
+@bot.command()
+async def import_turtle(ctx):
+    if ctx.channel.name == 'test':
+        role = get(ctx.guild.roles, name="Turtle")
+        if role is None:
+            role = await ctx.guild.create_role(name="Turtle")
+        await ctx.author.add_roles(role)
 
 bot.run(os.environ["TOKEN"])
